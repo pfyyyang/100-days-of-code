289173505942cc8d04b68be8899a665ca8935f71
pfyang <pfyyyang@gmail.com>
2026-07-06 21:45:59 +0800

    day8

--- day8/task1.py
+++ day8/task1.py
@@ -1,6 +1,21 @@
 def greet():
-    print("Hello World")
-    print("Hello World")
-    print("Hello World")
+    print("Hello pfyang")
+    print("How do you do Jack Bauer?")
+    print("Isn't the weather ")
 
-print(greet())
+greet()
+
+# Functions that allows for inputs
+
+# def my_function(something):
+#   Do this with something
+#   Then do this
+#   Finally do this
+# something = 123
+# something = parameter; 123 = argument
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+greet_with_name("Billie")
---
+++ day8/task2.py
@@ -0,0 +1,9 @@
+# Function with more than 1 input
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+# greet_with("Bill","Beijing")
+
+greet_with(name = "Bill", location = "London")
+# positional arguments
